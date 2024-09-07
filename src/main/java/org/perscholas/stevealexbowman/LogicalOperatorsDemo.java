2eb899312a74afdb70e9dde0e0a4f2bf509bf264
Steve Alex Bowman <stevealexbowman@gmail.com>
2024-09-07 17:45:00 -0400

    Created 7 Java Classes demonstrating Core Java Operators

---
+++ src/main/java/org/perscholas/stevealexbowman/LogicalOperatorsDemo.java
@@ -0,0 +1,14 @@
+package org.perscholas.stevealexbowman;
+
+public class LogicalOperatorsDemo {
+    public static void main(String[] args) {
+        boolean x = true;
+        boolean y = false;
+        System.out.println("x & y : " + (x & y));
+        System.out.println("x && y : " + (x && y));
+        System.out.println("x | y : " + (x | y));
+        System.out.println("x || y : " + (x || y));
+        System.out.println("x ^ y : " + (x ^ y));
+        System.out.println("!x : " + (!x));
+    }
+}
