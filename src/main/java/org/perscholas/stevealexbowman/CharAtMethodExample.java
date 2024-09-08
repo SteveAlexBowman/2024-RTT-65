c6471db016d784d1d1d7d72ac41a0a1a57ddfec7
Steve Alex Bowman <stevealexbowman@gmail.com>
2024-09-07 23:48:42 -0400

    Adding an example of using the .charAt() method within a print statement

---
+++ src/main/java/org/perscholas/stevealexbowman/CharAtMethodExample.java
@@ -0,0 +1,10 @@
+package org.perscholas.stevealexbowman;
+
+public class CharAtMethodExample {
+    public static void main(String[] args) {
+        String message = "Welcome to Java, Steve";
+        System.out.println("The first character in the message is: " + message.charAt(17));
+
+        // Displays the character: 'S' to the console
+    }
+}
