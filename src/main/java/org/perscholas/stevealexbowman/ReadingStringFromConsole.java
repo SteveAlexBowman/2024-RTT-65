01cc20605e3d99c8d65a5221a08a7114609ea389
Steve Alex Bowman <stevealexbowman@gmail.com>
2024-09-08 13:37:11 -0400

    Adding an example of reading a string from the console

---
+++ src/main/java/org/perscholas/stevealexbowman/ReadingStringFromConsole.java
@@ -0,0 +1,16 @@
+package org.perscholas.stevealexbowman;
+
+import java.util.Scanner;
+
+public class ReadingStringFromConsole {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter three word separated by spaces: ");
+        String s1 = input.next();
+        String s2 = input.next();
+        String s3 = input.next();
+        System.out.println("s1 is " + s1);
+        System.out.println("s2 is " + s2);
+        System.out.println("s3 is " + s3);
+    }
+}
--- src/main/java/org/perscholas/stevealexbowman/StringUpperExample.java
+++
@@ -1,9 +0,0 @@
-package org.perscholas.stevealexbowman;
-
-public class StringUpperExample {
-    public static void main(String[] args) {
-        String s1 = "hello how are you";  // Notice how the string is all lower case
-        String s1Upper = s1.toUpperCase();  // .toUpperCase() coverts the string to CAPITAL LETTERS when displayed
-        System.out.println(s1Upper);  // Displaying the string to the console
-    }
-}
