ca134cb93f591c2322739b739079d0de1a5cd320
Steve Alex Bowman <stevealexbowman@gmail.com>
2024-09-07 23:08:36 -0400

    Adding an example of using .toLowerCase() method

---
+++ src/main/java/org/perscholas/stevealexbowman/StringLowerExample.java
@@ -0,0 +1,9 @@
+package org.perscholas.stevealexbowman;
+
+public class StringLowerExample {
+    public static void main(String[] args) {
+        String s1 = "HELLO HOW Are You?";  // notice the capital HELLO HOW
+        String s1lower = s1.toLowerCase();  // the .toLowerCase() method ensures all characters are lowercase when displayed
+        System.out.println(s1lower);
+    }
+}
