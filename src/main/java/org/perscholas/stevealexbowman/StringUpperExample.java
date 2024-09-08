59ebc337e71b469916596165ef7e0711dfa137aa
Steve Alex Bowman <stevealexbowman@gmail.com>
2024-09-07 23:19:18 -0400

    Adding an example of using the .toUpperCase() method on a string

---
+++ src/main/java/org/perscholas/stevealexbowman/StringUpperExample.java
@@ -0,0 +1,9 @@
+package org.perscholas.stevealexbowman;
+
+public class StringUpperExample {
+    public static void main(String[] args) {
+        String s1 = "hello how are you";  // Notice how the string is all lower case
+        String s1Upper = s1.toUpperCase();  // .toUpperCase() coverts the string to CAPITAL LETTERS when displayed
+        System.out.println(s1Upper);  // Displaying the string to the console
+    }
+}
