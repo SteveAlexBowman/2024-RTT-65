73af9fd8473cbcd4b04c27a9afba857947825930
Steve Alex Bowman <stevealexbowman@gmail.com>
2024-09-18 15:40:54 -0400

    Created a program that creates an array of integers with a length of 3

---
+++ src/main/java/org/perscholas/stevealexbowman/Task1.java
@@ -0,0 +1,21 @@
+package org.perscholas.stevealexbowman;
+
+/*Task 1: Write a program that creates an array of integers with a length of 3.
+Assign the values 1, 2, and 3 to the indexes. Print out each array element.*/
+
+public class Task1 {
+    public static void main(String[] args) {
+
+        // Array of integers with a length of 3
+        int[] integers =  {1,2,3};
+
+        // print out each array element
+        System.out.println(integers[0]);
+        System.out.println(integers[1]);
+        System.out.println(integers[2]);
+
+
+
+
+    }
+}
