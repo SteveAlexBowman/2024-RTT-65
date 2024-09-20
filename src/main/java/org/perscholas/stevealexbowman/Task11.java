f4cc13698a74491ab3f50a0b6d50932d9569800d
Steve Alex Bowman <stevealexbowman@gmail.com>
2024-09-20 08:49:32 -0400

    Java program that asks a user to enter the number of favorite things they have and display those items to the console

--- src/main/java/org/perscholas/stevealexbowman/Task11.java
+++ src/main/java/org/perscholas/stevealexbowman/Task11.java
@@ -7,8 +7,36 @@ package org.perscholas.stevealexbowman;
     Finally, print out the contents of the array.
  */
 
+import java.util.Scanner;  // importing the library to use the scanner for user input
+
 public class Task11 {
     public static void main(String[] args) {
-        // TODO
+        // Create Scanner
+        Scanner userInput = new Scanner(System.in); // accepting the user's input from the console
+
+        // Display prompt to the user
+        System.out.println("How many favorite things do you own? Enter a whole number: ");
+
+        // Store the user's input into a int variable
+        int numberOfFavoriteThings = userInput.nextInt();// using method .nextInt() to ensure a whole number is entered
+        userInput.nextLine(); // consume newline left-over
+
+        // Creating an array the size of their numberOfFavoriteThings entered from the console
+        String [] favoriteThings = new String [numberOfFavoriteThings];
+
+        // Accepting the user's input from the console and storing that string in the array favoriteThings
+        for (int i = 0; i < numberOfFavoriteThings; i++) {
+            System.out.print("Enter favorite thing " + (i + 1) + ": ");
+            favoriteThings[i] = userInput.nextLine();
+        }
+
+        // Displaying the user's favorite things to the console using a for each loop
+        System.out.println("\nYour favorite things are: ");
+        for(String thing : favoriteThings) {
+            System.out.println(thing);
+        }
+
+        // Close the scanner
+        userInput.close();
     }
 }
