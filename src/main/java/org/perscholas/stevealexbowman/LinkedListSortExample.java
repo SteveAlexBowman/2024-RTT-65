9b1982221812830bfdcb0a07f7ca3b2e81b3c9a7
Steve Alex Bowman <stevealexbowman@gmail.com>
2024-09-30 22:47:37 -0400

    Adding an example of using LinkedList in Java using built-in methods

---
+++ src/main/java/org/perscholas/stevealexbowman/LinkedListSortExample.java
@@ -0,0 +1,31 @@
+package org.perscholas.stevealexbowman;
+
+import java.util.Collections;
+import java.util.LinkedList;
+
+public class LinkedListSortExample {
+    public static void main(String[] args) {
+
+        // Create Linked List
+        LinkedList<String> linkedList = new LinkedList<>();
+        linkedList.add("A");
+        linkedList.add("C");
+        linkedList.add("B");
+        linkedList.add("D");
+
+        // Unsorted
+        System.out.println(linkedList);
+
+        // 1. Sort the List
+        Collections.sort(linkedList);
+
+        // Sorted
+        System.out.println(linkedList);
+
+        // 2. Custom Sorting
+        Collections.sort(linkedList, Collections.reverseOrder());
+
+        // Custom Sorted
+        System.out.println(linkedList);
+    }
+}
